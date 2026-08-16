15fc2d17b153e1657e54ccbae9e953ae9bb375ed
Hemant Singh <hs2hemantsingh@gmail.com>
2026-08-17 01:05:16 +0530

    feat(client): integrate epoll in client

--- src/vpn_client.c
+++ src/vpn_client.c
@@ -19,7 +19,8 @@ int main(int argc, char *argv[]){
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(9999); // Example port
-    int inet_pton_val = inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr); // localhost for testing
+    //int inet_pton_val = inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr); // localhost for testing
+    int inet_pton_val = inet_pton(AF_INET, "10.1.1.20", &server_addr.sin_addr); // localhost for testing
 
     if (inet_pton_val <= 0) {
         if (inet_pton_val == 0)
@@ -38,19 +39,72 @@ int main(int argc, char *argv[]){
         close(fd);
         return 1;
     }
+
+     // Set up the epoll instance
+    struct epoll_event events[10];
+
+    struct epoll_event ev;
+
+    int epoll_fd =  epoll_create1(0);
+    if (epoll_fd == -1) {
+        perror("epoll_create1");
+    } 
+
+    ev.events = EPOLLIN;
+    ev.data.fd = udp_fd;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, udp_fd, &ev);
+
+    ev.events = EPOLLIN;
+    ev.data.fd = fd;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD,fd, &ev);
    
-
-    // Continuously read packets from the TUN device and send over the UDP socket  
+    
     while(1) {
-        ssize_t nread = read(fd, buffer, sizeof(buffer));
-        
-        if (nread < 0) {
-            perror("Read error");
-            break;
-        }
+        int n_ready_fds = epoll_wait(epoll_fd, events, 10, -1);
 
-        printf("\nReceived Packet (%zd bytes)\n", nread);
-        send(udp_fd, buffer, sizeof(buffer), 0);
+        for(int i=0; i<n_ready_fds; i++){
+            int ready_fd = events[i].data.fd;
+
+            if(ready_fd == fd && (events[i].events & EPOLLIN)){
+                // Read from the TUN device
+                ssize_t nread = read(ready_fd, buffer, sizeof(buffer));
+        
+                if (nread < 0) {
+                    perror("Read error");
+                    break;
+                }
+
+                printf("\nReceived Packet (%zd bytes)\n", nread);
+
+                // Send the packets over the UDP socket
+                send(udp_fd, buffer, nread, 0);
+                
+            }
+
+            if(ready_fd == udp_fd){
+                // Receive packets from UDP socket
+                ssize_t nread = recvfrom(ready_fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&server_addr, &(socklen_t){sizeof(server_addr)});
+                if (nread < 0) {
+                    perror("recvfrom error");
+                    break;
+                }
+
+                // Print all received bytes in hex
+                printf("Buffer (%zd bytes): ", nread);
+                for (int i = 0; i < nread; i++) {
+                    printf("%02x ", buffer[i]);
+                }
+                printf("\n");
+
+                // Write the received packet to the TUN device
+                ssize_t nwritten = write(fd, buffer, nread);
+                if (nwritten < 0) {
+                    perror("write to tun0 failed");
+                } else {
+                    printf("Wrote %zd bytes to tun0\n", nwritten);
+                }
+            }
+        }
     }
 
     close(fd);
